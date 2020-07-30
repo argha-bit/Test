2293dc438d955aa4e76701ed030664bd1add09ae
Argha Roy <argharoy.roy192@gmail.com>
2020-07-30 20:03:22 +0530

    First Commit

---
+++ Tree.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Map;
+public class Tree{
+public static void main(String[] args)
+{
+	NavigableMap<Integer,String> map = new TreeMap<Integer,String>();
+	map.put(101,"Argha");
+	map.put(105,"Roy");
+	map.put(103,"Mango");
+	map.put(102,"Apple");
+	System.out.println(map);
+	System.out.println(map.descendingMap());
+	System.out.println(map.headMap(102,true));
+	System.out.println(map.tailMap(103,true));
+}
+}
