4251377d01217d9a9363bc49009c76a1aedb63e9
Argha Roy <argharoy.roy192@gmail.com>
2020-07-30 20:48:40 +0530

    Third Commit

--- Tree.java
+++ Tree.java
@@ -8,6 +8,7 @@ public static void main(String[] args)
 	map.put(105,"Roy");
 	map.put(103,"Mango");
 	map.put(102,"Apple");
+map.put(104,"Orange");
 	System.out.println(map);
 	System.out.println(map.descendingMap());
 	System.out.println(map.headMap(102,true));
